82b0b40c95f45202b7c2b7045dfc4473fd46419d
Jordan <uberbomb@yahoo.com>
2022-08-15 22:02:25 -0400

    Add popup form
    Added popup form and basic Javascript to set visibility on button click.
    Syled form and touched up a few other elements.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+const test = new Book("test", "poopypants", "10000", true);
+const test2 = new Book("test2", "poopypants", "10000", true);
+const test3 = new Book("test3", "poopypants", "10000", true);
+const test4 = new Book("test4", "poopypants", "10000", true);
+
 const myLibrary = [test, test2, test3, test4];
 
 function Book(title, author, pages, read) {
@@ -17,20 +22,16 @@ function displayBooks() {
   })
 }
 
-const addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', () => {
-  addBooktoLibrary(test);
-  } 
-)
+const addBook = document.querySelector(".add-book");
+addBook.addEventListener('click', () => {
+  const popUpForm = document.querySelector(".form-container");
+  popUpForm.style.visibility = "visible";
+})
 
-const showBtn = document.getElementById('showBtn');
-showBtn.addEventListener('click', () => {
-  displayBooks();
-  } 
-)
-
-
-const test = new Book("test", "poopypants", "10000", true);
-const test2 = new Book("test2", "poopypants", "10000", true);
-const test3 = new Book("test3", "poopypants", "10000", true);
-const test4 = new Book("test4", "poopypants", "10000", true);
+const submitBook = document.querySelector(".submit-book");
+submitBook.addEventListener('click', () => {
+  const popUpForm = document.querySelector(".form-container");
+  const titleValue = document.getElementById("title").value;
+  console.log(titleValue);
+  popUpForm.style.visibility = "hidden";
+})
