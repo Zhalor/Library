1ecd968f96b4cf6b9afb866f0a677b6d15ec547b
Jordan <uberbomb@yahoo.com>
2022-08-16 21:57:51 -0400

    Add Javascript code to create book from popup form input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
-const test = new Book("test", "poopypants", "10000", true);
-const test2 = new Book("test2", "poopypants", "10000", true);
-const test3 = new Book("test3", "poopypants", "10000", true);
-const test4 = new Book("test4", "poopypants", "10000", true);
-
-const myLibrary = [test, test2, test3, test4];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,26 +7,62 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBooktoLibrary(book) {
+Book.prototype.addBooktoLibrary = function(book) {
   myLibrary.push(book);
 }
 
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    console.log(book.title);
-  })
+const deleteBookBtn = document.getElementById('delete-button-btn');
+
+
+const createCard = function() {
+  const cardDiv = document.createElement('div');
+  const titleParagraph = document.createElement('p');
+  const authorParagraph = document.createElement('p');
+  const pagesParagraph = document.createElement('p');
+  const readBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
+
+  cardDiv.className = "card";
+  cardDiv.setAttribute('data', myLibrary.length - 1);
+
+  const cardContainer = document.getElementById('card-container');
+
+  cardContainer.append(cardDiv);
+  cardDiv.append(titleParagraph);
+  cardDiv.append(authorParagraph);
+  cardDiv.append(pagesParagraph);
+  cardDiv.append(readBtn);
+  cardDiv.append(deleteBtn);
+
+  titleParagraph.innerText = myLibrary[0].title;
+  authorParagraph.innerText = myLibrary[0].author;
+  pagesParagraph.innerText = myLibrary[0].pages;
+  readBtn.innerText = "Read"
+  deleteBtn.innerText = "Delete";
+  deleteBtn.setAttribute('id', 'delete-button-btn')
 }
 
-const addBook = document.querySelector(".add-book");
-addBook.addEventListener('click', () => {
+addBook = function () {
+  const popUpForm = document.querySelector(".form-container");
+  const title = document.getElementById("title")
+  const author = document.getElementById("author")
+  const pages = document.getElementById("pages")
+  const read = document.getElementById("read")
+  const newBook = new Book(title.value, author.value, pages.value, read.checked);
+  newBook.addBooktoLibrary(newBook);
+  createCard();
+  popUpForm.style.visibility = "hidden";
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+}
+
+const addBookBtn = document.querySelector(".add-book");
+addBookBtn.addEventListener('click', () => {
   const popUpForm = document.querySelector(".form-container");
   popUpForm.style.visibility = "visible";
 })
 
-const submitBook = document.querySelector(".submit-book");
-submitBook.addEventListener('click', () => {
-  const popUpForm = document.querySelector(".form-container");
-  const titleValue = document.getElementById("title").value;
-  console.log(titleValue);
-  popUpForm.style.visibility = "hidden";
-})
+const submitBookBtn = document.querySelector(".submit-book");
+submitBookBtn.addEventListener('click', addBook);
