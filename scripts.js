3a65aeffa83096d50f341151179b2af76bde0fb3
Jordan <uberbomb@yahoo.com>
2022-08-19 21:25:49 -0400

    Add form validation and styling
    Page was reloading when form was submitted, so i set button to type
    "button" and created my own form validation. Added some styling in a few
    areas and removed placeholder card.

--- scripts.js
+++ scripts.js
@@ -13,10 +13,62 @@ Book.prototype.addBooktoLibrary = function(book) {
 
 const addBookBtn = document.querySelector(".add-book");
 const submitBookBtn = document.querySelector(".submit-book");
+const closeFormBtn = document.getElementById('close-popup-form');
+
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+
+const titleLabel = document.querySelector('label[for="title"]');
+const authorLabel = document.querySelector('label[for="author"]');
+const pagesLabel = document.querySelector('label[for="pages"]');
+
+titleInput.addEventListener('keyup', () => {
+  if(titleInput.value == "" || titleInput.value == null){
+    titleLabel.classList.add('error');
+    submitBookBtn.disabled = true;
+  } else {
+    titleLabel.classList.remove('error');
+    submitBookBtn.disabled = false;
+  }
+})
+
+authorInput.addEventListener('keyup', () => {
+  if(authorInput.value == "" || authorInput.value == null){
+    authorLabel.classList.add('error');
+    submitBookBtn.disabled = true;
+  } else {
+    authorLabel.classList.remove('error');
+    submitBookBtn.disabled = false;
+  }
+})
+
+pagesInput.addEventListener('keyup', () => {
+  if(pagesInput.value == "" || pagesInput.value == null){
+    pagesLabel.classList.add('error');
+    submitBookBtn.disabled = true;
+  } else {
+    pagesLabel.classList.remove('error');
+    submitBookBtn.disabled = false;
+  }
+})
 
 addBookBtn.addEventListener('click', () => {
   const popUpForm = document.querySelector(".form-container");
   popUpForm.style.visibility = "visible";
+})
+
+closeFormBtn.addEventListener('click', () => {
+  const popUpForm = document.querySelector(".form-container");
+  const title = document.getElementById("title")
+  const author = document.getElementById("author")
+  const pages = document.getElementById("pages")
+  const read = document.getElementById("read")
+  popUpForm.style.visibility = "hidden";
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
 })
 
 function addBook() {
@@ -68,7 +120,6 @@ function createCard() {
   } else {
     readBtn.innerText = "Not Read"
     readBtn.classList.add('not-read');
-    //Add class to change button color
   }
   readBtn.classList.add("read-book-btn");
   readBtn.addEventListener('click', toggleRead)
